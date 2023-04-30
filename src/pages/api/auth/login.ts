117280aca621c2381ec8b8a58d831bb9333a3838
Pavlo Karas <kar.pavel123@gmail.com>
2023-04-30 15:15:00 +0300

    Use trpc procedure to find uniq user

--- src/pages/api/auth/login.ts
+++ src/pages/api/auth/login.ts
@@ -1,14 +1,16 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { verifyPassword, generateToken } from "~/utils/auth/auth";
-import { type User, PrismaClient } from "@prisma/client";
+import type { User } from "@prisma/client";
+import { appRouter } from "~/server/api/root";
+import { prisma } from "~/server/db";
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
+export default async function login(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "POST") {
-    const prisma = new PrismaClient();
     const { login, password } = req.body as User;
+    const caller = appRouter.createCaller({ prisma });
 
     try {
-      const user = await prisma.user.findUnique({ where: { login: login } });
+      const user = await caller.auth.findUniqUser({ login });
 
       if (!user) {
         return res.status(401).json({ message: "Invalid email or password" });
@@ -24,10 +26,8 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       res.status(200).json({ message: "Login successful", token });
     } catch (error) {
       res.status(400).json({ message: "Error logging in", error });
-    } finally {
-      await prisma.$disconnect();
     }
   } else {
     res.status(405).json({ message: "Method not allowed" });
   }
-};
+}
