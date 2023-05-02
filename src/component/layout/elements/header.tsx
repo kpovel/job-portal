2d9ed281ade4559156de23507fadaef191568a3c
Pavlo Karas <kar.pavel123@gmail.com>
2023-05-02 14:17:05 +0300

    Extract profile dropdown menu into profileDropdownMenu.tsx

--- src/component/layout/elements/header.tsx
+++ src/component/layout/elements/header.tsx
@@ -1,15 +1,15 @@
-import { Fragment } from "react";
-import { Disclosure, Menu, Transition } from "@headlessui/react";
+import { Disclosure } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { ProfileDropdownMenu } from "~/component/layout/elements/profileDropdownMenu";
 
 const navigation: { name: string; href: string }[] = [
   { name: "Пропозиції", href: "/my/offers" },
   { name: "Вакансії", href: "/jobs" },
 ];
 
-function classNames(...classes: { toString: () => string }[]) {
+export function classNames(...classes: { toString: () => string }[]) {
   return classes.filter(Boolean).join(" ");
 }
 
@@ -67,76 +67,7 @@ export function Header() {
                     </div>
                   </div>
                 </div>
-                <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                  {/*todo: add profile status*/}
-
-                  {/* Profile dropdown */}
-                  <Menu as="div" className="relative ml-3">
-                    <div>
-                      <Menu.Button className="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                        <span className="sr-only">Open user menu</span>
-
-                        {/*todo: display first letter of First name and Last name*/}
-                        <span className="inline-flex h-10 w-10 items-center justify-center rounded-full bg-gray-500">
-                          <span className="font-medium leading-none text-white">
-                            KP
-                          </span>
-                        </span>
-                      </Menu.Button>
-                    </div>
-                    <Transition
-                      as={Fragment}
-                      enter="transition ease-out duration-100"
-                      enterFrom="transform opacity-0 scale-95"
-                      enterTo="transform opacity-100 scale-100"
-                      leave="transition ease-in duration-75"
-                      leaveFrom="transform opacity-100 scale-100"
-                      leaveTo="transform opacity-0 scale-95"
-                    >
-                      <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                        <Menu.Item>
-                          {({ active }) => (
-                            <Link
-                              href="#"
-                              className={classNames(
-                                active ? "bg-gray-100" : "",
-                                "block px-4 py-2 text-sm text-gray-700"
-                              )}
-                            >
-                              Your Profile
-                            </Link>
-                          )}
-                        </Menu.Item>
-                        <Menu.Item>
-                          {({ active }) => (
-                            <Link
-                              href="#"
-                              className={classNames(
-                                active ? "bg-gray-100" : "",
-                                "block px-4 py-2 text-sm text-gray-700"
-                              )}
-                            >
-                              Settings
-                            </Link>
-                          )}
-                        </Menu.Item>
-                        <Menu.Item>
-                          {({ active }) => (
-                            <Link
-                              href="#"
-                              className={classNames(
-                                active ? "bg-gray-100" : "",
-                                "block px-4 py-2 text-sm text-gray-700"
-                              )}
-                            >
-                              Sign out
-                            </Link>
-                          )}
-                        </Menu.Item>
-                      </Menu.Items>
-                    </Transition>
-                  </Menu>
-                </div>
+                <ProfileDropdownMenu />
               </div>
             </div>
             <Disclosure.Panel className="sm:hidden">
--- src/component/layout/layout.tsx
+++ src/component/layout/layout.tsx
@@ -1,12 +1,12 @@
 import { type ReactNode } from "react";
-import { Header } from "~/component/layout/header";
-import { Footer } from "~/component/layout/footer";
+import { Header } from "~/component/layout/elements/header";
+import { Footer } from "~/component/layout/elements/footer";
 
 export function Layout({ children }: { children: ReactNode }) {
   return (
     <div className="flex h-max min-h-screen flex-col bg-gray-50">
       <Header />
-      <main className="container mx-auto flex w-full grow flex-col p-4 ">
+      <main className="container mx-auto flex w-full grow flex-col p-4">
         {children}
       </main>
       <Footer />
