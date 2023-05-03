6a06d43c26e66494a4a952974f6e0adc198c0269
Pavlo Karas <kar.pavel123@gmail.com>
2023-05-03 15:55:25 +0300

    Create AuthContext

--- src/component/layout/layout.tsx
+++ src/component/layout/layout.tsx
@@ -1,11 +1,14 @@
-import { type ReactNode } from "react";
+import { type ReactNode, useContext } from "react";
 import { Header } from "~/component/layout/elements/header";
 import { Footer } from "~/component/layout/elements/footer";
+import { GuestOnlyHeader } from "~/component/layout/elements/guestOnlyHeader";
+import { AuthContext } from "~/utils/auth/authContext";
 
 export function Layout({ children }: { children: ReactNode }) {
+  const isAuthorizedUser = useContext(AuthContext);
   return (
     <div className="flex h-max min-h-screen flex-col bg-gray-50">
-      <Header />
+      {isAuthorizedUser ? <Header /> : <GuestOnlyHeader />}
       <main className="mx-auto flex w-full grow flex-col">{children}</main>
       <Footer />
     </div>
