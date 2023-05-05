4d35bac03d783b35ed18c3c7b64fd97d70360a71
Pavlo Karas <kar.pavel123@gmail.com>
2023-05-05 17:06:23 +0300

    Made input form reusable

--- src/pages/my/profile.tsx
+++ src/pages/my/profile.tsx
@@ -2,6 +2,74 @@ import { Layout } from "~/component/layout/layout";
 import { type FormEvent } from "react";
 import React from "react";
 import * as Tabs from "@radix-ui/react-tabs";
+import { FormInput } from "~/component/profileForm/formInput";
+
+export type FormInputConfig = {
+  label: string;
+  type: string;
+  name: string;
+  autoComplete: string;
+  id: string;
+};
+
+const formInputs: FormInputConfig[] = [
+  {
+    label: "Прізвище",
+    type: "text",
+    name: "first-name",
+    id: "first-name",
+    autoComplete: "given-name",
+  },
+  {
+    label: "Імʼя",
+    type: "text",
+    name: "last-name",
+    id: "last-name",
+    autoComplete: "family-name",
+  },
+  {
+    label: "Вік",
+    type: "text",
+    name: "company",
+    id: "company",
+    autoComplete: "organization",
+  },
+  {
+    label: "Номер телефону",
+    type: "tel",
+    name: "phone-number",
+    id: "phone-number",
+    autoComplete: "tel",
+  },
+  {
+    label: "Електронна пошта",
+    type: "email",
+    name: "email",
+    id: "email",
+    autoComplete: "email",
+  },
+  {
+    label: "Посилання на LinkedIn акаунт",
+    type: "url",
+    name: "url",
+    id: "LinedInUrl",
+    autoComplete: "LinkedIn",
+  },
+  {
+    label: "Посилання на GitHub акаунт",
+    type: "url",
+    name: "GitHubUrl",
+    id: "GitHubUrl",
+    autoComplete: "GitHub",
+  },
+  {
+    label: "Посилання на Telegram акаунт",
+    type: "url",
+    name: "TelegramUrl",
+    id: "TelegramUrl",
+    autoComplete: "Telegram",
+  },
+];
 
 export default function Profile() {
   function updateCandidateData(e: FormEvent) {
@@ -52,150 +120,21 @@ export default function Profile() {
               >
                 <form
                   onSubmit={updateCandidateData}
-                  className="mx-auto mt-16 max-w-xl sm:mt-20"
+                  className="mx-auto my-5 max-w-xl"
                 >
                   <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
-                    <div className="sm:col-span-2">
-                      <label
-                        htmlFor="first-name"
-                        className="block text-sm font-semibold leading-6 text-gray-900"
-                      >
-                        Прізвище
-                      </label>
-                      <div className="mt-2.5">
-                        <input
-                          type="text"
-                          name="first-name"
-                          id="first-name"
-                          autoComplete="given-name"
-                          className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    {formInputs.map((formInput) => {
+                      return (
+                        <FormInput
+                          key={formInput.id}
+                          type={formInput.type}
+                          label={formInput.label}
+                          name={formInput.name}
+                          id={formInput.id}
+                          autoComplete={formInput.autoComplete}
                         />
-                      </div>
-                    </div>
-                    <div className="sm:col-span-2">
-                      <label
-                        htmlFor="last-name"
-                        className="block text-sm font-semibold leading-6 text-gray-900"
-                      >
-                        Імʼя
-                      </label>
-                      <div className="mt-2.5">
-                        <input
-                          type="text"
-                          name="last-name"
-                          id="last-name"
-                          autoComplete="family-name"
-                          className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                        />
-                      </div>
-                    </div>
-                    <div className="sm:col-span-2">
-                      <label
-                        htmlFor="company"
-                        className="block text-sm font-semibold leading-6 text-gray-900"
-                      >
-                        Вік
-                      </label>
-                      <div className="mt-2.5">
-                        <input
-                          type="text"
-                          name="company"
-                          id="company"
-                          autoComplete="organization"
-                          className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                        />
-                      </div>
-                    </div>
-                    <div className="sm:col-span-2">
-                      <label
-                        htmlFor="phone-number"
-                        className="block text-sm font-semibold leading-6 text-gray-900"
-                      >
-                        Номер телефону
-                      </label>
-                      <div className="relative mt-2.5">
-                        <div className="absolute inset-y-0 left-0 flex items-center">
-                          <label htmlFor="country" className="sr-only">
-                            Country
-                          </label>
-                        </div>
-                        <input
-                          type="tel"
-                          name="phone-number"
-                          id="phone-number"
-                          autoComplete="tel"
-                          className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                        />
-                      </div>
-                    </div>
-                    <div className="sm:col-span-2">
-                      <label
-                        htmlFor="email"
-                        className="block text-sm font-semibold leading-6 text-gray-900"
-                      >
-                        Електронна пошта
-                      </label>
-                      <div className="mt-2.5">
-                        <input
-                          type="email"
-                          name="email"
-                          id="email"
-                          autoComplete="email"
-                          className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                        />
-                      </div>
-                    </div>
-                    <div className="sm:col-span-2">
-                      <label
-                        htmlFor="email"
-                        className="block text-sm font-semibold leading-6 text-gray-900"
-                      >
-                        Посилання на LinkedIn акаунт
-                      </label>
-                      <div className="mt-2.5">
-                        <input
-                          type="email"
-                          name="email"
-                          id="email"
-                          autoComplete="email"
-                          className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                        />
-                      </div>
-                    </div>
-                    <div className="sm:col-span-2">
-                      <label
-                        htmlFor="email"
-                        className="block text-sm font-semibold leading-6 text-gray-900"
-                      >
-                        Посилання на GitHub акаунт
-                      </label>
-                      <div className="mt-2.5">
-                        <input
-                          type="email"
-                          name="email"
-                          id="email"
-                          autoComplete="email"
-                          className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                        />
-                      </div>
-                    </div>
-                    <div className="sm:col-span-2">
-                      <label
-                        htmlFor="email"
-                        className="block text-sm font-semibold leading-6 text-gray-900"
-                      >
-                        Посилання на Telegram акаунт
-                      </label>
-                      <div className="mt-2.5">
-                        <input
-                          type="email"
-                          name="email"
-                          id="email"
-                          autoComplete="email"
-                          className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                        />
-                      </div>
-                    </div>
+                      );
+                    })}
                   </div>
                   <div className="mt-10">
                     <button
