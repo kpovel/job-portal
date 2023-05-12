5a6dff5fa20b431cf6b4dbdb01e1c9c7ae3be67c
Pavlo Karas <kar.pavel123@gmail.com>
2023-05-12 09:06:14 +0300

    Created the navigation menu for the employer profile

--- src/pages/home/profile.tsx
+++ src/pages/home/profile.tsx
@@ -9,10 +9,12 @@ import type {
   InferGetServerSidePropsType,
 } from "next";
 import superjson from "superjson";
-import type { User, Employer, Questionnaire, Vacancy } from "@prisma/client";
+import type { Employer, Questionnaire, User, Vacancy } from "@prisma/client";
 import React, { type ChangeEvent, type FormEvent, useState } from "react";
 import { FormInput } from "~/component/profileForm/formInput";
 import type { CandidateFields as UserFields } from "~/component/candidate/candidateAccountForm";
+import { EmployerNavigationMenu } from "~/component/employer/employerNavigationMenu";
+import Head from "next/head";
 
 type EmployerData =
   | User & {
@@ -22,7 +24,7 @@ type EmployerData =
           };
     };
 
-export default function EmployeeProfile({
+export default function EmployerProfile({
   employer,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   const parsedEmployerData: EmployerData = superjson.parse(employer);
@@ -50,12 +52,12 @@ export default function EmployeeProfile({
     }));
   }
 
-  function handleUpdateEmployeeProfile(e: FormEvent): void {
+  function handleUpdateEmployerProfile(e: FormEvent): void {
     e.preventDefault();
-    void updateEmployeeProfile();
+    void updateEmployerProfile();
   }
 
-  async function updateEmployeeProfile(): Promise<void> {
+  async function updateEmployerProfile(): Promise<void> {
     try {
       await fetch("/api/user/updateProfile", {
         method: "POST",
@@ -68,70 +70,75 @@ export default function EmployeeProfile({
   }
 
   return (
-    <Layout>
-      <div className="container mx-auto my-4 flex flex-col items-center space-y-8">
-        <h1 className="text-3xl font-bold">Мій профіль</h1>
-        <hr className="w-full border-gray-300" />
-        <form
-          onSubmit={handleUpdateEmployeeProfile}
-          className="w-full max-w-md rounded-lg bg-white p-8 shadow-lg"
-        >
-          <div className="space-y-4">
-            <FormInput
-              label="Імʼя"
-              id="firstName"
-              name="firstName"
-              autoComplete="given-name"
-              type="text"
-              value={formData.firstName}
-              onChange={handleUpdateForm}
-            />
-            <FormInput
-              label="Прізвище"
-              id="lastName"
-              name="lastName"
-              autoComplete="given-name"
-              type="text"
-              value={formData.lastName}
-              onChange={handleUpdateForm}
-            />
-            <FormInput
-              label="phoneNumber"
-              id="phoneNumber"
-              name="phoneNumber"
-              autoComplete="tel"
-              type="text"
-              value={formData.phoneNumber}
-              onChange={handleUpdateForm}
-            />
-            <FormInput
-              label="email"
-              id="email"
-              name="email"
-              autoComplete="email"
-              type="email"
-              value={formData.email}
-              onChange={handleUpdateForm}
-            />
-            <FormInput
-              label="linkedinLink"
-              id="linkedinLink"
-              name="linkedinLink"
-              autoComplete="linkedinLink"
-              type="url"
-              value={formData.linkedinLink}
-              onChange={handleUpdateForm}
-            />
-          </div>
-          <button
-            type="submit"
-            className="mt-6 block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+    <>
+      <Head>
+        <title>Job Portal – Мій профіль</title>
+      </Head>
+      <Layout>
+        <div className="container mx-auto my-4 flex flex-col items-center space-y-8">
+          <EmployerNavigationMenu />
+          <hr className="w-full border-gray-300" />
+          <form
+            onSubmit={handleUpdateEmployerProfile}
+            className="w-full max-w-md rounded-lg bg-white p-8 shadow-lg"
           >
-            Оновити дані мого профілю
-          </button>
-        </form>
-      </div>
-    </Layout>
+            <div className="space-y-4">
+              <FormInput
+                label="Імʼя"
+                id="firstName"
+                name="firstName"
+                autoComplete="given-name"
+                type="text"
+                value={formData.firstName}
+                onChange={handleUpdateForm}
+              />
+              <FormInput
+                label="Прізвище"
+                id="lastName"
+                name="lastName"
+                autoComplete="given-name"
+                type="text"
+                value={formData.lastName}
+                onChange={handleUpdateForm}
+              />
+              <FormInput
+                label="phoneNumber"
+                id="phoneNumber"
+                name="phoneNumber"
+                autoComplete="tel"
+                type="text"
+                value={formData.phoneNumber}
+                onChange={handleUpdateForm}
+              />
+              <FormInput
+                label="email"
+                id="email"
+                name="email"
+                autoComplete="email"
+                type="email"
+                value={formData.email}
+                onChange={handleUpdateForm}
+              />
+              <FormInput
+                label="linkedinLink"
+                id="linkedinLink"
+                name="linkedinLink"
+                autoComplete="linkedinLink"
+                type="url"
+                value={formData.linkedinLink}
+                onChange={handleUpdateForm}
+              />
+            </div>
+            <button
+              type="submit"
+              className="mt-6 block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Оновити дані мого профілю
+            </button>
+          </form>
+        </div>
+      </Layout>
+    </>
   );
 }
 
