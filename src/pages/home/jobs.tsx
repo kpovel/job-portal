f1bd9af86bd6225ef2f1eea406e8ae6dda7cb44e
Pavlo Karas <kar.pavel123@gmail.com>
2023-05-13 12:22:44 +0300

    Added a view of created jobs by the employer

---
+++ src/pages/home/jobs.tsx
@@ -0,0 +1,79 @@
+import { Layout } from "~/component/layout/layout";
+import Head from "next/head";
+import { EmployerNavigationMenu } from "~/component/employer/employerNavigationMenu";
+import React from "react";
+import type {
+  GetServerSidePropsContext,
+  InferGetServerSidePropsType,
+} from "next";
+import { getEmployerData } from "~/utils/getEmployerData/getEmployerData";
+import type { EmployerData } from "~/pages/home/profile";
+import superjson from "superjson";
+
+export default function Jobs({
+  employer,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  const parsedEmployerData: EmployerData = superjson.parse(employer);
+  const employerVacancies = parsedEmployerData.employer.questionnaires;
+
+  return (
+    <>
+      <Head>
+        <title>Job Portal - Мої вакансії</title>
+      </Head>
+      <Layout>
+        <div className="container mx-auto my-4 flex flex-col items-center space-y-8">
+          <EmployerNavigationMenu />
+          <hr className="w-full border-gray-300" />
+          <div className="flex gap-5">
+            {employerVacancies.length ? (
+              employerVacancies.map((questionnaire) => (
+                <div
+                  key={questionnaire.questionnaireId}
+                  className="mb-4 rounded-lg bg-white p-6 shadow-lg"
+                >
+                  <div className="mb-4 flex items-center align-top">
+                    <h2 className="grow text-xl font-bold text-gray-800">
+                      {questionnaire.vacancy.specialty}
+                    </h2>
+                    {questionnaire.vacancy.salary && (
+                      <p className="font-semibold text-gray-700">
+                        ${questionnaire.vacancy.salary}
+                      </p>
+                    )}
+                  </div>
+                  <div>
+                    <p className="mb-2 text-gray-600">
+                      {questionnaire.vacancy.requirements}
+                    </p>
+                    <p className="text-gray-600">
+                      {questionnaire.vacancy.conditions}
+                    </p>
+                  </div>
+                </div>
+              ))
+            ) : (
+              <h2 className="text-xl font-bold text-gray-800">
+                Ви ще не створили жодної вакансії
+              </h2>
+            )}
+
+            {/*todo: create a new vacancy*/}
+            <button
+              type="submit"
+              className="block w-full self-start rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Створити вакансію
+            </button>
+          </div>
+        </div>
+      </Layout>
+    </>
+  );
+}
+
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  return await getEmployerData(context);
+};
