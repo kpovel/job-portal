3e5fbd887029daa771696a40efec0516f04392cf
Pavlo Karas <kar.pavel123@gmail.com>
2023-06-18 18:42:00 +0300

    added the requirement for the employer to select a job before sending an offer

--- src/component/candidate/sendJobOffer.tsx
+++ src/component/candidate/sendJobOffer.tsx
@@ -133,7 +133,7 @@ export function SendJobOffer({ candidateId }: { candidateId: string }) {
             type="submit"
             className={`mt-6 block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600
             ${
-              isSentOffer || !isFormFieldOut
+              isSentOffer || !isFormFieldOut || !selectedVacancy
                 ? "cursor-not-allowed opacity-50"
                 : ""
             }`}
