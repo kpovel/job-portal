021247656a4032c06665785ce8ca63ee254d42f5
Pavlo Karas <kar.pavel123@gmail.com>
2023-09-17 19:51:46 +0300

    refactor: rewrite findUserByToken to raw sql

--- src/pages/api/auth/findUserByToken.ts
+++ src/pages/api/auth/findUserByToken.ts
@@ -1,29 +1,30 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { appRouter } from "~/server/api/root";
-import { prisma } from "~/server/db";
+import { dbClient } from "~/server/db";
 import { verifyToken } from "~/utils/auth/auth";
 import { type VerifyToken } from "~/utils/auth/withoutAuth";
 
 export default async function findUserByToken(
   req: NextApiRequest,
-  res: NextApiResponse
+  res: NextApiResponse,
 ) {
   if (req.method === "POST") {
     const { authToken } = req.body as { authToken: string };
 
     try {
-      const verifiedToken = verifyToken(authToken) as VerifyToken;
+      const verifiedToken = verifyToken(authToken) as VerifyToken | null;
 
       if (!verifiedToken) {
         res.status(401).json({ message: "Invalid token key" });
       }
 
-      const caller = appRouter.createCaller({ prisma });
-      const authorizedUser = await caller.auth.findUserById({
-        id: verifiedToken.userId,
-      });
+      const authorizedUser = await dbClient.execute(
+        "select id from User where id = :userId;",
+        {
+          userId: verifiedToken?.userId,
+        },
+      );
 
-      if (!authorizedUser) {
+      if (!authorizedUser.rows.length) {
         res.status(404).json({ message: "User is not find" });
       }
 
