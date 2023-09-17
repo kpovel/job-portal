4a1feff3dd5b6c8bf65926648d988e70505de405
Pavlo Karas <kar.pavel123@gmail.com>
2023-09-17 22:02:38 +0300

    fix: update type import

--- src/utils/auth/authContext.tsx
+++ src/utils/auth/authContext.tsx
@@ -1,4 +1,4 @@
 import { createContext } from "react";
-import type { User } from ".prisma/client";
+import type { User } from "../dbSchema/user";
 
 export const AuthContext = createContext<User | null>(null);
