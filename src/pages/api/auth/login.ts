4e20aab648248a20bcaae380424ba4a6ffbd2258
Pavlo Karas <kar.pavel123@gmail.com>
2023-09-22 19:13:56 +0300

    feat: rewrite login raw sql

--- src/pages/api/auth/login.ts
+++ src/pages/api/auth/login.ts
@@ -1,33 +1,38 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { verifyPassword, generateToken } from "~/utils/auth/auth";
-import type { User } from "@prisma/client";
-import { appRouter } from "~/server/api/root";
-import { prisma } from "~/server/db";
+import { dbClient } from "~/server/db";
+import type { User } from "~/utils/dbSchema/user";
 
 export default async function login(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === "POST") {
-    const { login, password } = req.body as User;
-    const caller = appRouter.createCaller({ prisma });
-
-    try {
-      const user = await caller.auth.findUserByLogin({ login });
-
-      if (!user) {
-        return res.status(401).json({ message: "Invalid email or password" });
-      }
-
-      const isPasswordValid = await verifyPassword(password, user.password);
-
-      if (!isPasswordValid) {
-        return res.status(401).json({ message: "Invalid email or password" });
-      }
-
-      const token = generateToken(user.id);
-      res.status(200).json({ message: "Login successful", token });
-    } catch (error) {
-      res.status(400).json({ message: "Error logging in", error });
-    }
-  } else {
+  if (req.method !== "POST") {
     res.status(405).json({ message: "Method not allowed" });
+  }
+
+  const { login, password } = req.body as { login: string; password: string };
+
+  try {
+    const findUserQuery = await dbClient.execute(
+      "select * from User where login = :login",
+      {
+        login,
+      },
+    );
+
+    const user = findUserQuery.rows[0] as User | undefined;
+
+    if (!user) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+
+    const isPasswordValid = await verifyPassword(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Password doesn't match" });
+    }
+
+    const token = generateToken(user.id);
+    res.status(200).json({ message: "Login successful", token });
+  } catch (error) {
+    res.status(400).json({ message: "Error logging in", error });
   }
 }
