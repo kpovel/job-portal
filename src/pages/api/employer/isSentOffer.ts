ab98ce0b47759a50c0f7a1592ae53e82b5b033e8
Pavlo Karas <kar.pavel123@gmail.com>
2023-09-23 22:12:06 +0300

    feat: rewrite if send job offer to raw sql

--- src/pages/api/employer/isSentOffer.ts
+++ src/pages/api/employer/isSentOffer.ts
@@ -1,30 +1,28 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { appRouter } from "~/server/api/root";
-import { prisma } from "~/server/db";
+import { dbClient } from "~/server/db";
 
 export default async function reviewJobOffer(
   req: NextApiRequest,
-  res: NextApiResponse
+  res: NextApiResponse,
 ) {
   if (req.method !== "POST") {
     res.status(405).json({ message: "Method not allowed" });
   }
 
   try {
-    const {
-      candidateId,
-      employerId
-    } = req.body as {candidateId: string, employerId: string};
+    const { candidateId, employerId } = req.body as {
+      candidateId: string;
+      employerId: string;
+    };
 
-    const caller = appRouter.createCaller({ prisma });
-    const sentOffer = await caller.sendOffer.isSentOffer({
-      candidateId,
-      employerId
-    });
+    const sentOfferQuery = await dbClient.execute(
+      "select * from Response where candidateId = :candidateId and employerId = :employerId and responseBy = 'EMPLOYER'",
+      { candidateId, employerId },
+    );
 
     res.status(200).json({
       message: "Successfully reviewed job offer",
-      sentOffer,
+      sentOffer: sentOfferQuery.rows,
     });
   } catch (error) {
     res.status(400).json({ message: "Something went wrong", error });
