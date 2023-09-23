33bcde74792bee8a9f9806e5cd5356c14df4c91e
Pavlo Karas <kar.pavel123@gmail.com>
2023-09-23 21:58:40 +0300

    feat: creating job vacancies to raw sql

--- src/pages/api/employer/createVacancy.ts
+++ src/pages/api/employer/createVacancy.ts
@@ -1,11 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { appRouter } from "~/server/api/root";
-import { prisma } from "~/server/db";
+import { dbClient } from "~/server/db";
 import type { VacancyFields } from "~/pages/home/create-vacancy";
+import { createId } from "@paralleldrive/cuid2";
+import type { Vacancy } from "dbSchema/models";
 
 export default async function updateProfile(
   req: NextApiRequest,
-  res: NextApiResponse
+  res: NextApiResponse,
 ) {
   if (req.method !== "POST") {
     res.status(405).json({ message: "Method not allowed" });
@@ -23,16 +24,30 @@ export default async function updateProfile(
       employment,
     } = req.body as { employerId: string } & VacancyFields;
 
-    const caller = appRouter.createCaller({ prisma });
-    const createdVacancy = await caller.employer.createVacancy({
-      employerId,
-      specialty,
-      salary,
-      duties,
-      requirements,
-      conditions,
-      workSchedule,
-      employment,
+    const createdVacancy = await dbClient.transaction(async (tx) => {
+      const questionnaireId = createId();
+      await tx.execute(
+        "insert into Questionnaire (questionnaireId, questionnaireType, employerId) values (:questionnaireId, 'VACANCY', :employerId);",
+        { questionnaireId, employerId },
+      );
+
+      const vacancyQuery = await tx.execute(
+        `insert into Vacancy (questionnaireId, specialty, salary, duties, requirements, conditions, workSchedule, employment, employerId)
+        values (:questionnaireId, :specialty, :salary, :duties, :requirements, :conditions, :workSchedule, :employment, :employerId);`,
+        {
+          questionnaireId,
+          specialty,
+          salary,
+          duties,
+          requirements,
+          conditions,
+          workSchedule,
+          employment,
+          employerId,
+        },
+      );
+
+      return vacancyQuery.rows[0] as Vacancy;
     });
 
     res.status(200).json({
@@ -40,6 +55,6 @@ export default async function updateProfile(
       createdVacancy,
     });
   } catch (error) {
-    res.status(400).json({ message: "Something went wrong", error });
+    res.status(400).json({ message: "Error to create a vacancy", error });
   }
 }
