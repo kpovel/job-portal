e38ba129a915135ddf879ca8e29facd8229e31f2
Pavlo Karas <kar.pavel123@gmail.com>
2023-09-28 21:45:18 +0300

    feat: rewrite updating company data to raw sql

--- src/pages/api/employer/updateCompanyData.ts
+++ src/pages/api/employer/updateCompanyData.ts
@@ -1,10 +1,9 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { appRouter } from "~/server/api/root";
-import { prisma } from "~/server/db";
+import { dbClient } from "~/server/db";
 
 export default async function updateProfile(
   req: NextApiRequest,
-  res: NextApiResponse
+  res: NextApiResponse,
 ) {
   if (req.method !== "POST") {
     res.status(405).json({ message: "Method not allowed" });
@@ -17,18 +16,19 @@ export default async function updateProfile(
       companyAddress: string;
     };
 
-    const caller = appRouter.createCaller({ prisma });
-    const updatedCompanyData = await caller.employer.updateCompanyData({
-      employerId,
-      companyName,
-      companyAddress,
-    });
+    await dbClient.execute(
+      "update Employer set companyName = :companyName, companyAddress = :companyAddress where employerId = :employerId;",
+      {
+        companyName,
+        companyAddress,
+        employerId,
+      },
+    );
 
     res.status(200).json({
       message: "Successful update user profile",
-      updatedCompanyData,
     });
   } catch (error) {
-    res.status(400).json({ message: "Something went wrong", error });
+    res.status(400).json({ message: "Failed to update company data", error });
   }
 }
