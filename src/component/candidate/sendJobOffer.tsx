61aef4affec77b0865617eb28d19429e7caa9a54
Pavlo Karas <kar.pavel123@gmail.com>
2023-09-28 21:32:26 +0300

    feat: rewrite sending job offer to raw sql

--- src/component/candidate/sendJobOffer.tsx
+++ src/component/candidate/sendJobOffer.tsx
@@ -13,7 +13,7 @@ export function SendJobOffer({ candidateId }: { candidateId: string }) {
 
   function setCurrentVacancy(questionnaireId: string) {
     const selectedVacancy = availableVacancies.find(
-      (vacancy) => vacancy.questionnaireId === questionnaireId
+      (vacancy) => vacancy.questionnaireId === questionnaireId,
     );
     setSelectedVacancy(selectedVacancy || null);
   }
@@ -108,19 +108,7 @@ export function SendJobOffer({ candidateId }: { candidateId: string }) {
         }),
       });
 
-      type SuccessSentOffer = {
-        message: string;
-        createdResponse: Response;
-      };
-
-      type ErrorResponse = {
-        message: string;
-        error: string;
-      };
-
-      type OfferResponse = SuccessSentOffer | ErrorResponse;
-      const offerResponse = (await sentOffer.json()) as OfferResponse;
-      if ("createdResponse" in offerResponse) {
+      if (sentOffer.ok) {
         setIsSentOffer(true);
         setOfferDescription("");
       }
--- src/pages/api/employer/sendOffer.ts
+++ src/pages/api/employer/sendOffer.ts
@@ -1,9 +1,10 @@
+import { createId } from "@paralleldrive/cuid2";
 import type { NextApiRequest, NextApiResponse } from "next";
-import { prisma } from "~/server/db";
+import { dbClient } from "~/server/db";
 
 export default async function reviewJobOffer(
   req: NextApiRequest,
-  res: NextApiResponse
+  res: NextApiResponse,
 ) {
   if (req.method !== "POST") {
     res.status(405).json({ message: "Method not allowed" });
@@ -18,24 +19,39 @@ export default async function reviewJobOffer(
         vacancyId: string;
       };
 
-    const candidatesResume = await prisma.questionnaire.findUnique({
-      where: { candidateId },
-    });
-
-    const createdResponse = await prisma.response.create({
-      data: {
-        employerId,
+    const resumeQuery = await dbClient.execute(
+      "select questionnaireId from Questionnaire where candidateId = :candidateId;",
+      {
         candidateId,
-        resumeId: candidatesResume?.questionnaireId || "",
-        coverLetter: offerDescription,
-        vacancyId,
-        responseBy: "EMPLOYER",
       },
-    });
+    );
+
+    const candidateResume = resumeQuery.rows[0] as
+      | { questionnaireId: string }
+      | undefined;
+
+    if (!candidateResume) {
+      res.status(404).json({
+        message: "Candidate resume not found",
+      });
+      return;
+    }
+
+    await dbClient.execute(
+      `insert into Response (resumeId, vacancyId, candidateId, employerId, coverLetter, responseId, responseBy)
+      VALUES (:resumeId, :vacancyId, :candidateId, :employerId, :coverLetter, :responseId, 'EMPLOYER');`,
+      {
+        resumeId: candidateResume.questionnaireId,
+        vacancyId,
+        candidateId,
+        employerId,
+        coverLetter: offerDescription,
+        responseId: createId(),
+      },
+    );
 
     res.status(200).json({
       message: "Successfully submitted the offer",
-      createdResponse,
     });
   } catch (error) {
     res.status(400).json({ message: "Something went wrong", error });
