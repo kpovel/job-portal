1992534e359eefdf460e1d11900fd78015732043
Pavlo Karas <kar.pavel123@gmail.com>
2023-09-28 21:55:35 +0300

    feat: rewrite updating moderation status of vacancy data to raw sql

--- src/pages/api/employer/updateModerationStatus.ts
+++ src/pages/api/employer/updateModerationStatus.ts
@@ -1,11 +1,10 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { appRouter } from "~/server/api/root";
-import { prisma } from "~/server/db";
+import { dbClient } from "~/server/db";
 import type { ModerationStatus } from "@prisma/client";
 
 export default async function updateModerationStatus(
   req: NextApiRequest,
-  res: NextApiResponse
+  res: NextApiResponse,
 ) {
   if (req.method !== "POST") {
     res.status(405).json({ message: "Method not allowed" });
@@ -17,17 +16,18 @@ export default async function updateModerationStatus(
       moderationStatus: ModerationStatus;
     };
 
-    const caller = appRouter.createCaller({ prisma });
-    const updatedVacancy = await caller.employer.updateModerationStatus({
-      moderationStatus,
-      questionnaireId,
-    });
+    await dbClient.execute(
+      "update Vacancy set moderationStatus = :moderationStatus where questionnaireId = :questionnaireId;",
+      {
+        moderationStatus,
+        questionnaireId,
+      },
+    );
 
     res.status(200).json({
       message: "Moderation status successfully updated",
-      updatedVacancy,
     });
   } catch (error) {
-    res.status(400).json({ message: "Something went wrong", error });
+    res.status(400).json({ message: "Failed to update moderation status", error });
   }
 }
