584f7c652345fc0b3892b9c4f1f2e42a14e3b4aa
Pavlo Karas <kar.pavel123@gmail.com>
2023-09-29 21:54:29 +0300

    feat: rewrite updating user profile data to raw sql

--- src/pages/api/user/updateProfile.ts
+++ src/pages/api/user/updateProfile.ts
@@ -1,11 +1,10 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import type { CandidateFields } from "~/component/candidate/candidateAccountForm";
-import { appRouter } from "~/server/api/root";
-import { prisma } from "~/server/db";
+import { dbClient } from "~/server/db";
 
 export default async function updateProfile(
   req: NextApiRequest,
-  res: NextApiResponse
+  res: NextApiResponse,
 ) {
   if (req.method !== "POST") {
     res.status(405).json({ message: "Method not allowed" });
@@ -24,21 +23,32 @@ export default async function updateProfile(
       email,
     } = req.body as CandidateFields & { id: string };
 
-    const caller = appRouter.createCaller({ prisma });
-    const updatedCandidateProfile = await caller.user.updateUserProfile({
-      id,
-      firstName,
-      lastName,
-      age,
-      githubLink,
-      linkedinLink,
-      telegramLink,
-      phoneNumber,
-      email,
-    });
+    await dbClient.execute(
+      `update User
+      set firstName    = :firstName,
+        lastName     = :lastName,
+        age          = :age,
+        githubLink   = :githubLink,
+        linkedinLink = :linkedinLink,
+        telegramLink = :telegramLink,
+        phoneNumber  = :phoneNumber,
+        email        = :email
+      where id = :id;`,
+      {
+        id,
+        firstName,
+        lastName,
+        age,
+        githubLink,
+        linkedinLink,
+        telegramLink,
+        phoneNumber,
+        email,
+      },
+    );
+
     res.status(200).json({
       message: "Successful update user profile",
-      updatedCandidateProfile,
     });
   } catch (error) {
     res.status(400).json({ message: "Something went wrong", error });
