2c70836975043ca33222fc8d5210f0ab07b74f42
Pavlo Karas <kar.pavel123@gmail.com>
2023-10-26 15:45:06 +0300

    fix: choose the right type

--- src/pages/api/employer/updateModerationStatus.ts
+++ src/pages/api/employer/updateModerationStatus.ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { dbClient } from "~/server/db";
-import type { ModerationStatus } from "@prisma/client";
+import type { ModerationStatus } from "~/utils/dbSchema/enums";
 
 export default async function updateModerationStatus(
   req: NextApiRequest,
