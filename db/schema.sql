f3c7c20ea67e6c86e0579f5359867a767c510162
Pavlo Karas <thepavlokaras@gmail.com>
2024-03-04 10:25:30 +0200

    refactor: db schema, add foreign and primary keys

--- db/schema.sql
+++ db/schema.sql
@@ -1,86 +1,70 @@
+drop table if exists FeedbackResult;
+drop table if exists Response;
+drop table if exists Vacancy;
+drop table if exists Resume;
+drop table if exists Candidate;
+drop table if exists Employer;
+drop table if exists User;
+
+-- uuid - 36
+
+create table User (
+    id            int unsigned primary key auto_increment,
+    user_uuid     char(36)                              not null,
+    user_type     enum ('CANDIDATE','EMPLOYER','ADMIN') not null,
+    login         varchar(191) unique                   not null,
+    password      varchar(191)                          not null,
+
+    last_name     varchar(191),
+    first_name    varchar(191),
+    middle_name   varchar(191), -- delete?
+    age           varchar(191),
+    phone_number  varchar(191),
+    email         varchar(191) unique,
+    linkedin_link varchar(191),
+    github_link   varchar(191),
+    telegram_link varchar(191)
+);
+
 create table Candidate (
-    candidateId varchar(191) not null,
-    unique key Candidate_candidateId_key (candidateId)
-);
+    candidate_id int unsigned primary key,
 
-create table Employer (
-    employerId     varchar(191) not null,
-    companyName    varchar(191),
-    companyAddress varchar(191) default null,
-    unique key Employer_employerId_key (employerId)
-);
-
-create table FeedbackResult (
-    responseId       varchar(191)                 not null,
-    response         mediumtext                   not null,
-    responseDate     datetime                     not null default current_timestamp,
-    feedbackResultId varchar(191)                 not null,
-    responseResult   enum ('ACCEPTED','REJECTED') NOT NULL,
-    primary key (feedbackResultId),
-    unique key FeedbackResult_responseId_key (responseId)
-);
-
-create table Questionnaire (
-    questionnaireId   varchar(191) primary key,
-    questionnaireType enum ('RESUME','VACANCY') not null,
-    candidateId       varchar(191),
-    employerId        varchar(191),
-    unique key Questionnaire_candidateId_key (candidateId),
-    key Questionnaire_employerId_idx (employerId)
-);
-
-create table Response (
-    resumeId     varchar(191) primary key,
-    vacancyId    varchar(191)                  not null,
-    candidateId  varchar(191)                  not null,
-    employerId   varchar(191)                  not null,
-    coverLetter  mediumtext                    not null,
-    responseDate datetime                      not null default current_timestamp,
-    responseId   varchar(191)                  not null,
-    responseBy   enum ('CANDIDATE','EMPLOYER') not null,
-    key Response_candidateId_idx (candidateId),
-    key Response_employerId_idx (employerId),
-    key Response_vacancyId_idx (vacancyId)
+    foreign key (candidate_id) references User (id)
 );
 
 create table Resume (
-    questionnaireId  varchar(191)                           not null,
-    moderationStatus enum ('PENDING','ACCEPTED','REJECTED') not null default 'PENDING',
-    workExperience   varchar(191),
-    skills           varchar(191),
-    education        varchar(191),
-    foreignLanguages varchar(191),
-    interests        varchar(191),
-    achievements     varchar(191),
-    specialty        varchar(191),
-    desiredSalary    varchar(191),
-    employment       varchar(191),
-    updatedAt        datetime                               not null default (now()) on update current_timestamp,
-    candidateId      varchar(191)                           not null,
-    unique key Resume_questionnaireId_key (questionnaireId),
-    unique key Resume_candidateId_key (candidateId)
+    id                int unsigned primary key auto_increment,
+    resume_uuid       char(36)                               not null,
+    candidate_id      int unsigned                           not null,
+
+    moderation_status enum ('PENDING','ACCEPTED','REJECTED') not null default 'PENDING',
+    work_experience   varchar(191),
+    skills            varchar(191),
+    education         varchar(191),
+    foreign_languages varchar(191),
+    interests         varchar(191),
+    achievements      varchar(191),
+    specialty         varchar(191),
+    desired_salary    varchar(191),
+    employment        varchar(191),
+    updated_at        datetime                               not null default now() on update current_timestamp,
+
+    foreign key (candidate_id) references Candidate (candidate_id)
 );
 
-create table User (
-    id           varchar(191) primary key,
-    userType     enum ('CANDIDATE','EMPLOYER','ADMIN') not null,
-    lastName     varchar(191),
-    firstName    varchar(191),
-    middleName   varchar(191),
-    age          varchar(191),
-    phoneNumber  varchar(191),
-    email        varchar(191),
-    linkedinLink varchar(191),
-    githubLink   varchar(191),
-    telegramLink varchar(191),
-    login        varchar(191)                          not null,
-    password     varchar(191)                          not null,
-    unique key User_login_key (login),
-    unique key User_email_key (email)
+create table Employer (
+    employer_id     int unsigned primary key,
+    company_name    varchar(191),
+    company_address varchar(191),
+
+    foreign key (employer_id) references User (id)
 );
 
 create table Vacancy (
-    questionnaireId   varchar(191)                           not null,
+    id                int unsigned primary key auto_increment,
+    vacancy_uuid      char(36)                               not null,
+    employer_id       int unsigned                           not null,
+
     specialty         varchar(191)                           not null,
     salary            varchar(191),
     duties            varchar(191),
@@ -89,9 +73,37 @@ create table Vacancy (
     workSchedule      mediumtext,
     employment        varchar(191),
     dateOfPublication datetime                               not null default current_timestamp,
-    employerId        varchar(191)                           not null,
     moderationStatus  enum ('PENDING','ACCEPTED','REJECTED') not null default 'PENDING',
-    unique key Vacancy_questionnaireId_key (questionnaireId),
-    key Vacancy_employerId_idx (employerId)
+
+    foreign key (employer_id) references Employer (employer_id)
 );
 
+create table Response (
+    id            int unsigned primary key auto_increment,
+    response_uuid char(36)                      not null,
+
+    candidate_id  int unsigned                  not null,
+    employer_id   int unsigned                  not null,
+    vacancy_id    int unsigned                  not null,
+    response_by   enum ('CANDIDATE','EMPLOYER') not null,
+
+    cover_letter  mediumtext                    not null,
+    response_date datetime                      not null default current_timestamp,
+
+    foreign key (candidate_id) references Candidate (candidate_id),
+    foreign key (employer_id) references Employer (employer_id),
+    foreign key (vacancy_id) references Vacancy (id)
+);
+
+
+create table FeedbackResult (
+    id                   int unsigned primary key auto_increment,
+    feedback_result_uuid char(36) unique              not null,
+    response_id          int unsigned              not null,
+
+    response             mediumtext                   not null,
+    responseDate         datetime                     not null default current_timestamp,
+    responseResult       enum ('ACCEPTED','REJECTED') not null,
+
+    foreign key (response_id) references Response (id)
+);
