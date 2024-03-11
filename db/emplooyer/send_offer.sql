2b862300e8269f4a221401fc0e281ab20972f3e0
Pavlo Karas <thepavlokaras@gmail.com>
2024-03-11 21:53:15 +0200

    fix: verify that an employer has sent a job offer to a candidate before inserting it to the response table

--- db/emplooyer/send_offer.sql
+++ db/emplooyer/send_offer.sql
@@ -1,3 +1,9 @@
+select id
+from response
+where employer_id = :employer_id
+  and candidate_id = (select id from user where user_uuid = :candidate_uuid)
+  and response_by_user_type_id = (select id as response_by_user_type_id from user_type where type = 'EMPLOYER');
+
 insert into response (response_uuid, candidate_id, employer_id, vacancy_id, cover_letter, response_by_user_type_id)
 values (:response_uuid,
         (select id from user where user_uuid = :candidate_uuid),
@@ -5,6 +11,3 @@ values (:response_uuid,
         (select id from vacancy where vacancy_uuid = :vacancy_id),
         :cover_letter,
         (select id from user_type where type = 'EMPLOYER'));
-
-select *
-from response;
--- src/pages/api/employer/sendOffer.ts
+++ src/pages/api/employer/sendOffer.ts
@@ -32,6 +32,23 @@ export default async function reviewJobOffer(
       vacancyId: string;
     };
 
+    const employerResponses = await dbClient.execute({
+      sql: "\
+select id \
+from response \
+where employer_id = :employer_id\
+  and candidate_id = (select id from user where user_uuid = :candidate_uuid)\
+  and response_by_user_type_id = (select id as response_by_user_type_id from user_type where type = 'EMPLOYER');",
+      args: {
+        employer_id: verifiedToken.userId,
+        candidate_uuid: candidateUUID,
+      },
+    });
+
+    if (employerResponses.rows.length) {
+      res.status(409).send("You have already made an offer to the user");
+    }
+
     await dbClient.execute({
       sql: "\
 insert into response (response_uuid, candidate_id, employer_id, vacancy_id, cover_letter, response_by_user_type_id)\
